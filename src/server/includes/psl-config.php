9c9f6cf21c9fbcd26945a8007a845f4ebcefe1fe
Jordan Emslie <jordems@gmail.com>
2018-02-16 12:29:03 -0800

    Starting Login Sessions

---
+++ src/server/includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "sec_user");    // The database username. 
+define("PASSWORD", "AcD1Qb349FP");    // The database password. 
+define("DATABASE", "secure_login");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "user");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
