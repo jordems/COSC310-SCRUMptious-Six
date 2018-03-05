971816e319bf240c2b8bc08cec2b1aabfabffb89
Jordan Emslie <jordems@gmail.com>
2018-03-05 13:47:53 -0800

    Small fixes

--- src/client/php/account.php
+++ src/client/php/account.php
@@ -2,25 +2,33 @@
 include_once 'includes/updateUser.php';
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
+
+sec_session_start();
+
+if (login_check($mysqli) == false) {
+    // If already Logged in then send to login page
+    header('Location:index.php');
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>SCRUMptious</title>
     <meta charset="utf-8">
-    <link href="../css/styles.css" rel="stylesheet" type="text/css" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <header>
    <div id="upper">
    <img src="images/logo.svg" alt="Logo" id="logo" />
-   <div class="dropdown"> 
-		<button class="dropbtn">User's Name</button> 
+   <div class="dropdown">
+		<button class="dropbtn"><?php echo $_SESSION['username'];?></button>
 		<div class="dropdown-content">
 			<p><a href="account.php">Account</a></p>
-			<p><a href="logout.jsp">Logout</a></p>
+			<p><a href="includes/logout.php">Logout</a></p>
 		</div>
-	</div> 
+	</div>
    </div>
    <div id="lower">
     <nav>
@@ -35,20 +43,21 @@ include_once 'includes/functions.php';
 </header>
 <main>
     <?php
-    //Need to pull users info and add to the form field (via value="") so they see their info when accessing profile
-    $sql = "SELECT * FROM Users Where user_id = ?";
+    //Need to pull users ifo and add to the form field (via value="") so they see their info when accessing profile
+    $sql = "SELECT email, firstName, lastName, address FROM Users Where uid = ?";
     ?>
-    <form action="includes/updateUser.php" method="post" name="update_form" onsubmit="return regformhash(this, this.username, this.email, this.firstName, this.lastName, this.address);">
-        <fieldset>
-                <legend>Update your Account Information</legend>
-                <label for="username">Username:</label> <input type='text' name='username' id='username'/>
-                <label for="email">Email:</label> <input type="text" name="email" id="email" />
-                <label for="firstName">First Name:</label> <input type="text" name="firstName" id="firstName" />
-                <label for="lastName">Last Name:</label> <input type="text" name="lastName" id="lastName" />
-                <label for="address">Address:</label> <input type="text" name="address" id="address" />
-                <input type="submit" value="Update" id="update-submit">
-        </fieldset>
-    </form>
+    <section id="center-nocolumns">
+      <form action="includes/updateUser.php" method="post" name="update_form" id="update-form">
+          <fieldset>
+                  <legend>Update your Account Information</legend>
+                  <label for="email">Email:</label> <input type="text" name="email" id="email" value=""/>
+                  <label for="firstName">First Name:</label> <input type="text" name="firstName" id="firstName" value=""/>
+                  <label for="lastName">Last Name:</label> <input type="text" name="lastName" id="lastName" value=""/>
+                  <label for="address">Address:</label> <input type="text" name="address" id="address" value=""/>
+                  <input type="submit" value="Update" id="update-submit">
+          </fieldset>
+      </form>
+    </section>
 </main>
 <footer>
      <p><a href="#">ABOUT US</a> | <a href="#">CONTACT US</a> | <a href="#">PRIVACY POLICY</a> | <a href="#">TERMS OF USE</a> | <a href="#">SUPPORT</a></p>
